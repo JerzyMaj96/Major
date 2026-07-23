71ae6bc392368bffa7223e30a8052170f9f86fa2
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-07-23 22:57:04 +0200

    refactor(backend): update Label class and Task class

--- backend/src/main/java/com/jerzymaj/major/models/Label.java
+++ backend/src/main/java/com/jerzymaj/major/models/Label.java
@@ -1,9 +1,11 @@
 package com.jerzymaj.major.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
+@Table(name = "labels")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,4 +14,18 @@ import lombok.*;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
 public class Label {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    @ToString.Include
+    private String name;
+
+    @Column(nullable = false)
+    private String color;
 }
