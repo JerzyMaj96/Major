3187027b4f9b9a6d3866a55ddba3133e4631af38
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-07-24 10:43:22 +0200

    fix(backend): add missing @Component annotation

--- backend/src/main/java/com/jerzymaj/major/security/AuthFacadeImpl.java
+++ backend/src/main/java/com/jerzymaj/major/security/AuthFacadeImpl.java
@@ -2,7 +2,9 @@ package com.jerzymaj.major.security;
 
 import com.jerzymaj.major.models.User;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthFacadeImpl implements AuthFacade {
     @Override
     public User getCurrentUser() {
