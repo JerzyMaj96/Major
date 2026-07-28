9b0c3667c5c1dbb5da835e57b66ef3a429ac1650
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-07-28 11:30:28 +0200

    - chore(backend-test): chage the secret key to a 512 bit one - test(backend-auth): add integration tests for AuthCotroller - fix(backend-exception): add exception handling for MethodArgumentNotValid and change the order of exception handling

--- backend/src/main/java/com/jerzymaj/major/exceptions/CustomResponseEntityExceptionHandler.java
+++ backend/src/main/java/com/jerzymaj/major/exceptions/CustomResponseEntityExceptionHandler.java
@@ -1,22 +1,26 @@
 package com.jerzymaj.major.exceptions;
 
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorDetails> handleAuthenticationException(AuthenticationException ex, WebRequest request) {
 
-        return buildResponse(ex, request, HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildResponse(ex, request, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
@@ -35,6 +39,29 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
     public final ResponseEntity<ErrorDetails> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
 
         return buildResponse(ex, request, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(fe -> fe.getField() + " - " + fe.getDefaultMessage())
+                .reduce((a, b) -> a + ", " + b)
+                .orElse("Validation failed");
+
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), message, request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
+
+        return buildResponse(ex, request, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private ResponseEntity<ErrorDetails> buildResponse(Exception ex, WebRequest request, HttpStatus status) {
---
+++ backend/src/test/java/com/jerzymaj/major/integration_tests/AuthControllerIntegration.java
@@ -0,0 +1,84 @@
+package com.jerzymaj.major.integration_tests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jerzymaj.major.Dtos.LoginRequest;
+import com.jerzymaj.major.Dtos.RegisterUserDto;
+import com.jerzymaj.major.services.UserService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+public class AuthControllerIntegration {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private UserService userService;
+
+    @BeforeEach
+    public void setup() {
+        userService.registerUser(new RegisterUserDto("tester", "tester@example.com", "password"));
+    }
+
+    @Test
+    public void loginWithValidCredentials_returnsToken() throws Exception {
+
+        LoginRequest loginRequest = new LoginRequest("tester", "password");
+
+        mockMvc.perform(post("/major/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(not(emptyString())));
+    }
+
+    @Test
+    public void loginWithInvalidPassword_returnsUnauthorized() throws Exception {
+        LoginRequest loginRequest = new LoginRequest("tester", "badpassword");
+
+        mockMvc.perform(post("/major/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void loginWithNonExistentUser_returnsUnauthorized() throws Exception {
+        LoginRequest request = new LoginRequest("nonexistent", "password");
+
+        mockMvc.perform(post("/major/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void loginWithBlankFields_returnsBadRequest() throws Exception {
+        LoginRequest request = new LoginRequest("", "");
+
+        mockMvc.perform(post("/major/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+}
