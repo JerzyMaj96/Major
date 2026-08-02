dccae8163cb54bcd4ad0f1e0eb83be7b20355121
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-02 22:49:46 +0200

    feat(backend-controller): create new LabelController class with all necessary endpoints

---
+++ backend/src/main/java/com/jerzymaj/major/controllers/LabelController.java
@@ -0,0 +1,66 @@
+package com.jerzymaj.major.controllers;
+
+import com.jerzymaj.major.Dtos.CreateLabelDto;
+import com.jerzymaj.major.Dtos.LabelDto;
+import com.jerzymaj.major.Dtos.UpdateLabelDto;
+import com.jerzymaj.major.mappers.LabelMapper;
+import com.jerzymaj.major.models.Label;
+import com.jerzymaj.major.services.LabelService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("major/api/labels")
+@RequiredArgsConstructor
+public class LabelController {
+
+    private final LabelService labelService;
+
+    @PostMapping
+    public ResponseEntity<LabelDto> createLabel(@Valid @RequestBody CreateLabelDto createLabelDto) {
+
+        LabelDto labelDto = LabelMapper.toDto(labelService.createLabel(createLabelDto));
+
+        return ResponseEntity.created(URI.create("major/api/labels/" + labelDto.id())).body(labelDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LabelDto>> retrieveAllLabels() {
+
+        List<LabelDto> labelDtoList = labelService.getAllLabels().stream()
+                .map(LabelMapper::toDto)
+                .toList();
+
+        return ResponseEntity.ok(labelDtoList);
+    }
+
+    @GetMapping("/{labelId}")
+    public ResponseEntity<LabelDto> retrieveLabelById(@PathVariable("labelId") Long labelId) {
+
+        LabelDto labelDto = LabelMapper.toDto(labelService.getLabelById(labelId));
+
+        return ResponseEntity.ok(labelDto);
+    }
+
+    @PatchMapping("/{labelId}")
+    public ResponseEntity<LabelDto> updateLabel(@PathVariable("labelId") Long labelId,
+                                                @Valid @RequestBody UpdateLabelDto updateLabelDto) {
+
+        LabelDto labelDto = LabelMapper.toDto(labelService.updateLabel(labelId, updateLabelDto));
+
+        return ResponseEntity.ok(labelDto);
+    }
+
+    @DeleteMapping("/{labelId}")
+    public ResponseEntity<Void> deleteLabel(@PathVariable("labelId") Long labelId) {
+
+        labelService.deleteLabelById(labelId);
+
+        return ResponseEntity.noContent().build();
+    }
+}
