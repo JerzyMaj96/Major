a6ab93a5dabde7ab73cd4ce38f8229ac5909d3c5
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-04 16:14:44 +0200

    test(backend): add necessary mock GptService and @ActivePrifiles(test) annotation on main test class

--- backend/src/test/java/com/jerzymaj/major/MajorApplicationTests.java
+++ backend/src/test/java/com/jerzymaj/major/MajorApplicationTests.java
@@ -1,10 +1,17 @@
 package com.jerzymaj.major;
 
+import com.jerzymaj.major.services.GptService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MajorApplicationTests {
+
+	@MockitoBean
+	private GptService gptService;
 
 	@Test
 	void contextLoads() {
