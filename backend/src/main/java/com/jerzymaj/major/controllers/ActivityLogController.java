4359ee8b8254c16416e0d9e880fb22321d093521
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-04 11:30:01 +0200

    feat(backend): create new ActivityLogController containing only one endpoint for retrieving all activity logs

---
+++ backend/src/main/java/com/jerzymaj/major/controllers/ActivityLogController.java
@@ -0,0 +1,26 @@
+package com.jerzymaj.major.controllers;
+
+import com.jerzymaj.major.Dtos.ActivityLogDto;
+import com.jerzymaj.major.mappers.ActivityLogMapper;
+import com.jerzymaj.major.services.ActivityLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("major/api")
+@RequiredArgsConstructor
+public class ActivityLogController {
+
+    private final ActivityLogService activityLogService;
+
+    @GetMapping("/activity-logs")
+    public List<ActivityLogDto> retrieveAllActivityLogs() {
+        return activityLogService.findAllActivityLogs().stream()
+                .map(ActivityLogMapper::toDto)
+                .toList();
+    }
+}
