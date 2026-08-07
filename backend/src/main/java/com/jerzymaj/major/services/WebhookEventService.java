4e37afe98dff0604d433f35dadb9af115c341dfc
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-07 14:09:21 +0200

    chore(backend-webhook): add GitHub webhook secret configuration property

---
+++ backend/src/main/java/com/jerzymaj/major/services/WebhookEventService.java
@@ -0,0 +1,12 @@
+package com.jerzymaj.major.services;
+
+import com.jerzymaj.major.repos.WebhookEventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WebhookEventService {
+
+    private final WebhookEventRepository webhookEventRepository;
+}
