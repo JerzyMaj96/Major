b51ac382321a681016ee4ed567196a7d99118ea6
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-08 11:53:23 +0200

    feat(backend-webhook service): create new extractTaskIdFromRef method

--- backend/src/main/java/com/jerzymaj/major/services/WebhookEventService.java
+++ backend/src/main/java/com/jerzymaj/major/services/WebhookEventService.java
@@ -4,11 +4,26 @@ import com.jerzymaj.major.repos.WebhookEventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 @RequiredArgsConstructor
 public class WebhookEventService {
 
     private final WebhookEventRepository webhookEventRepository;
+
+    private Optional<Long> extractTaskIdFromRef(String ref) {
+        Pattern pattern = Pattern.compile("task/(\\d+)");
+        Matcher matcher = pattern.matcher(ref);
+
+        if (matcher.find()) {
+            return Optional.of(Long.parseLong(matcher.group(1)));
+        }
+
+        return Optional.empty();
+    }
 
     public void processEvent(String eventType, String payload) {
 
