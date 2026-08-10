35b4087cc13444381b3fdce368f1e24e9ef49239
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-10 19:12:23 +0200

    feat(backend-activity-log-service): create new getAllActivityLogsLastWeek method

--- backend/src/main/java/com/jerzymaj/major/services/ActivityLogService.java
+++ backend/src/main/java/com/jerzymaj/major/services/ActivityLogService.java
@@ -39,4 +39,8 @@ public class ActivityLogService {
                 .task(task)
                 .build());
     }
+
+    public List<ActivityLog> getAllActivityLogsLastWeek() {
+        return activityLogRepository.findAllLastWeek();
+    }
 }
