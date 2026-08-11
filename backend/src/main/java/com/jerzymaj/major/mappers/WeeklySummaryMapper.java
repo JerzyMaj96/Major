840ff479f8008e698449292f16e59e5b4b6d401c
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-11 12:40:37 +0200

    feat(backend-mapper): create new WeeklySummaryMapper

--- backend/src/main/java/com/jerzymaj/major/mappers/WeeklySummaryMapper.java
+++ backend/src/main/java/com/jerzymaj/major/mappers/WeeklySummaryMapper.java
@@ -1,4 +1,23 @@
 package com.jerzymaj.major.mappers;
 
-public class WeeklySummaryMapper {
+import com.jerzymaj.major.Dtos.WeeklySummaryDto;
+import com.jerzymaj.major.models.WeeklySummary;
+
+public final class WeeklySummaryMapper {
+
+    private WeeklySummaryMapper() {
+    }
+
+    public static WeeklySummaryDto toDto(WeeklySummary weeklySummary) {
+
+        return new WeeklySummaryDto(
+          weeklySummary.getId(),
+          weeklySummary.getContent(),
+          weeklySummary.getTasksCompleted(),
+          weeklySummary.getTasksCreated(),
+          weeklySummary.getPeriodStart(),
+          weeklySummary.getPeriodEnd(),
+          weeklySummary.getGeneratedAt()
+        );
+    }
 }
