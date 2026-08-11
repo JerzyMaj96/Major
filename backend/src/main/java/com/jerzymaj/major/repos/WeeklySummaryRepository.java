5fc371bad20e701de78448b952c8cf42f337b470
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-11 11:08:25 +0200

    fix(backend-weeklysummary-repo): change method name from created to generated

--- backend/src/main/java/com/jerzymaj/major/repos/WeeklySummaryRepository.java
+++ backend/src/main/java/com/jerzymaj/major/repos/WeeklySummaryRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface WeeklySummaryRepository extends JpaRepository<WeeklySummary, Long> {
 
-    Optional<WeeklySummary> findFirstByOrderByCreatedAtDesc();
+    Optional<WeeklySummary> findFirstByOrderByGeneratedAtDesc();
 }
