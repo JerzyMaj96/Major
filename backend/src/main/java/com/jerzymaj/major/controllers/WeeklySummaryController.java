1ffa63bc56f15b38d361fc0c5ce6bf597a31dc9e
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-11 17:21:10 +0200

    fix(backend-weeklysummary-controller): fix endpoint routes and request mapping route

--- backend/src/main/java/com/jerzymaj/major/controllers/WeeklySummaryController.java
+++ backend/src/main/java/com/jerzymaj/major/controllers/WeeklySummaryController.java
@@ -1,6 +1,7 @@
 package com.jerzymaj.major.controllers;
 
 import com.jerzymaj.major.Dtos.WeeklySummaryDto;
+import com.jerzymaj.major.configuration.ApiRoutes;
 import com.jerzymaj.major.mappers.WeeklySummaryMapper;
 import com.jerzymaj.major.services.WeeklySummaryService;
 import lombok.RequiredArgsConstructor;
@@ -12,18 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/major/api/weekly-summary")
+@RequestMapping(ApiRoutes.BASE_API + "/weekly-summary")
 @RequiredArgsConstructor
 public class WeeklySummaryController {
 
     private final WeeklySummaryService weeklySummaryService;
 
-    @GetMapping("last-week")
+    @GetMapping("/last-week")
     public ResponseEntity<WeeklySummaryDto> getWeeklySummary() {
         return ResponseEntity.ok(WeeklySummaryMapper.toDto(weeklySummaryService.getLastWeeksSummary()));
     }
 
-    @GetMapping("all")
+    @GetMapping("/all")
     public ResponseEntity<List<WeeklySummaryDto>> getAllWeeklySummary() {
         return ResponseEntity.ok(weeklySummaryService.getAllWeeklySummary().stream()
                 .map(WeeklySummaryMapper::toDto)
