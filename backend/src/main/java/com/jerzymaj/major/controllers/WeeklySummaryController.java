8803a574b9842c697f9bed481e04c98e04130eed
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-11 11:58:26 +0200

    feat(backend): crsate new WeeklySummaryController with getWeeklySummary and getAllWeeklySummary endpoints

---
+++ backend/src/main/java/com/jerzymaj/major/controllers/WeeklySummaryController.java
@@ -0,0 +1,28 @@
+package com.jerzymaj.major.controllers;
+
+import com.jerzymaj.major.models.WeeklySummary;
+import com.jerzymaj.major.services.WeeklySummaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/major/api/weekly-summary")
+@RequiredArgsConstructor
+public class WeeklySummaryController {
+
+    private final WeeklySummaryService weeklySummaryService;
+
+    @GetMapping("last-week")
+    public List<WeeklySummary> getWeeklySummary() {
+        return weeklySummaryService.getLastWeeksSummary();
+    }
+
+    @GetMapping("all")
+    public List<WeeklySummary> getAllWeeklySummary() {
+        return weeklySummaryService.getAllWeeklySummary();
+    }
+}
