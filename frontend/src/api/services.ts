940ec01e421933ecbf839d1bb1ab1ba2525f5166
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-14 21:57:27 +0200

    feat(frontend-services): create new services.ts file with authService that provides register, login and getCurrentUser functions

---
+++ frontend/src/api/services.ts
@@ -0,0 +1,28 @@
+import type { User, UserRegister } from "../types/types";
+import { authFetch, baseUrl } from "./api_helper";
+
+export const authService = {
+  register: async (userData: UserRegister): Promise<User> => {
+    const response = await fetch(`${baseUrl}/major/api/users/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    });
+    if (!response.ok) throw new Error(await response.text());
+    return response.json() as Promise<User>;
+  },
+  login: async (name: string, password: string) => {
+    const response = await fetch(`${baseUrl}/major/api/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, password }),
+    });
+    if (!response.ok) throw new Error(await response.text());
+    return response.text();
+  },
+  getCurrentUser: async (): Promise<User> => {
+    const response = await authFetch("GET", `/major/api/users/me`);
+    if (!response.ok) throw new Error("Failed to get user");
+    return response.json() as Promise<User>;
+  },
+};
