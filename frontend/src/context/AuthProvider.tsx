e3ace546051bd009e7fb16c9a8e70aee7f681be8
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-15 05:45:35 +0200

    feat(frontend): implement AuthProvider for authentication state management

---
+++ frontend/src/context/AuthProvider.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState, type ReactNode } from "react";
+import { getAuthToken, setAuthToken } from "../api/api_helper";
+import { authService } from "../api/services";
+import type { User } from "../types/types";
+import { AuthContext } from "./AuthContext";
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const initAuth = async () => {
+      const token = getAuthToken();
+      if (token) {
+        try {
+          const userData: User = await authService.getCurrentUser();
+          setUser(userData);
+        } catch (ex) {
+          console.error("Auth check failed", ex);
+          setAuthToken(null);
+        }
+      }
+      setLoading(false);
+    };
+    initAuth();
+  }, []);
+
+  const login = async (username: string, password: string) => {
+    const token = await authService.login(username, password);
+    setAuthToken(token);
+    const userData: User = await authService.getCurrentUser();
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setAuthToken(null);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
