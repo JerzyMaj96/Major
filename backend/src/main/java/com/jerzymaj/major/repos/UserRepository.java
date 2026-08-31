0e560307c6d0d12bb4e5a5f775bd106a2d67aee4
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-08-31 14:34:37 +0200

    feat(backend-user-repo): add findByName method back

--- backend/src/main/java/com/jerzymaj/major/repos/UserRepository.java
+++ backend/src/main/java/com/jerzymaj/major/repos/UserRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByName(String username);
+
     Optional<User> findByNameOrEmail(String username, String email);
 
     boolean existsByName(String username);
