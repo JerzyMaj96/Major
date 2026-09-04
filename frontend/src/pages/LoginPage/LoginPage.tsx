a55636c5f5ed46d7fe3e056b521a19c144cc8f8d
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-09-04 16:50:38 +0200

    feat(frontend-pages): create new LoginPage

---
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,54 @@
+import type { UserLogin } from "../../types/types";
+import { useFormState } from "../../hooks/useFormState";
+import { useNavigate } from "react-router";
+import { useAuth } from "../../hooks/useAuth";
+
+function LoginPage() {
+  const { values: credentials, handleChange } = useFormState<UserLogin>({
+    identifier: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+  const {login} = useAuth();
+
+  const handleSubmit = async (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      await login(credentials);
+      navigate("/dashboard");
+    } catch (error) {
+      if (error instanceof Error) {
+        alert("Error: " + error.message);
+      } else {
+        alert("An unknown error occurred");
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="identifier"
+          placeholder="Username"
+          value={credentials.identifier}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
