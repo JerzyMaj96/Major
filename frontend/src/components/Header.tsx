f8fc583045a47a1b4a0e2b9d5bad6088a4ec4dd4
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-09-10 07:34:53 +0200

    feat(frontend): add initial Header component implementation

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { userService } from "../api/services";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { user, logout } = useAuth();
+
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+  const closeMenu = () => setMenuOpen(false);
+
+  const handleDeleteAccount = async () => {
+    if (!window.confirm(`Would you like to delete your account?`)) {
+      return closeMenu();
+    }
+
+    try {
+      await userService.deleteAccount();
+      alert("Account deleted successfully");
+      logout();
+    } catch (error) {
+      if (error instanceof Error) {
+        alert("Error: " + error.message);
+      } else {
+        alert("An unknown error occurred");
+      }
+    }
+  };
+
+  return (
+    <header>
+      <h1>MAJOR</h1>
+
+      {user && (
+        <div>
+          <div className="user-box" onClick={toggleMenu}>
+            <AccountCircleIcon />
+            <p className="user-name">Hello, {user.name}!</p>
+            <div>
+              {menuOpen && (
+                <div className="user-menu">
+                  <button
+                    onClick={() => {
+                      logout();
+                      closeMenu();
+                    }}
+                  >
+                    <LogoutIcon className="logout-icon" /> Log out
+                  </button>
+                  <button onClick={handleDeleteAccount}>Delete Account</button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </header>
+  );
+}
+
+export default Header;
