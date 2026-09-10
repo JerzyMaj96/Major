2dcc7d02fdb2039750ba1e5001656768cebd98bc
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-09-10 07:23:44 +0200

    refactor(frontend): separete authService into authService and userService for fontend-backend complience
    feat(frontend): add deleteAccount function

--- frontend/src/api/services.ts
+++ frontend/src/api/services.ts
@@ -2,15 +2,6 @@ import type { User, UserLogin, UserRegister } from "../types/types";
 import { authFetch, baseUrl } from "./api_helper";
 
 export const authService = {
-  register: async (userData: UserRegister): Promise<User> => {
-    const response = await fetch(`${baseUrl}/major/api/users/register`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(userData),
-    });
-    if (!response.ok) throw new Error(await response.text());
-    return response.json() as Promise<User>;
-  },
   login: async (credentials: UserLogin): Promise<string> => {
     const response = await fetch(`${baseUrl}/major/api/auth/login`, {
       method: "POST",
@@ -20,9 +11,25 @@ export const authService = {
     if (!response.ok) throw new Error(await response.text());
     return response.text();
   },
+};
+
+export const userService = {
+  register: async (userData: UserRegister): Promise<User> => {
+    const response = await fetch(`${baseUrl}/major/api/users/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userData),
+    });
+    if (!response.ok) throw new Error(await response.text());
+    return response.json() as Promise<User>;
+  },
   getCurrentUser: async (): Promise<User> => {
     const response = await authFetch("GET", `/major/api/users/me`);
     if (!response.ok) throw new Error("Failed to get user");
     return response.json() as Promise<User>;
+  },
+  deleteAccount: async (): Promise<void> => {
+    const response = await authFetch("DELETE", `/major/api/users/delete-me`);
+    if (!response.ok) throw new Error("Failed to delete account");
   },
 };
