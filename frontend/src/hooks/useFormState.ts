093de0011baca93c9735aa910f0abf2b4c6d29f6
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-09-12 00:03:02 +0200

    refactor(frontend): add an alternative event argument type - React.ChangeEvent<HTMLTextAreaElement>

--- frontend/src/hooks/useFormState.ts
+++ frontend/src/hooks/useFormState.ts
@@ -3,7 +3,7 @@ import { useState } from "react";
 export function useFormState<T>(initialState: T) {
   const [values, setValues] = useState<T>(initialState);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
     const { name, value } = event.target;
     setValues((prev) => ({ ...prev, [name]: value }));
   };
