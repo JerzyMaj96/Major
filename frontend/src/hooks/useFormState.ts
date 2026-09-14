b7b9db8ca4e7b0e3f63ce0099b17ee15672cafff
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-09-14 09:31:49 +0200

    fix(frontend): addapt useFormState to properly handle checkbox input

--- frontend/src/hooks/useFormState.ts
+++ frontend/src/hooks/useFormState.ts
@@ -4,8 +4,14 @@ export function useFormState<T>(initialState: T) {
   const [values, setValues] = useState<T>(initialState);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { name, value } = event.target;
-    setValues((prev) => ({ ...prev, [name]: value }));
+    const { name, value, type } = event.target;
+
+    if (type === "checkbox") {
+      const checked = (event.target as HTMLInputElement).checked;
+      setValues((prev) => ({ ...prev, [name]: checked }));
+    } else {
+      setValues((prev) => ({ ...prev, [name]: value }));
+    }
   };
 
   return { values, setValues, handleChange };
