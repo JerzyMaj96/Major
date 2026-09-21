cd990717fef8a997fb2024d5cfbc3169259e3a26
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-09-21 11:26:26 +0200

    refactor(frontend): relmove no longer necessary event target type conditional

--- frontend/src/hooks/useFormState.ts
+++ frontend/src/hooks/useFormState.ts
@@ -3,15 +3,14 @@ import { useState } from "react";
 export function useFormState<T>(initialState: T) {
   const [values, setValues] = useState<T>(initialState);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { name, value, type } = event.target;
+  const handleChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = event.target;
 
-    if (type === "checkbox") {
-      const checked = (event.target as HTMLInputElement).checked;
-      setValues((prev) => ({ ...prev, [name]: checked }));
-    } else {
-      setValues((prev) => ({ ...prev, [name]: value }));
-    }
+    setValues((prev) => ({ ...prev, [name]: value }));
   };
 
   return { values, setValues, handleChange };
