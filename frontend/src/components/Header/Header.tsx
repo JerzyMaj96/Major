e9b34764bbcca1ad493c23583167ccb7878a8699
JerzyMaj96 <jerzy.maj96@gmail.com>
2026-09-21 19:48:26 +0200

    feat(frontend): create pages menu that navigates to the right page, and add css styling

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,17 +4,28 @@ import { useAuth } from "../../hooks/useAuth";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { userService } from "../../api/services";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
   const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  const toggleMenu = () => setMenuOpen((prev) => !prev);
-  const closeMenu = () => setMenuOpen(false);
+  const [pagesMenuOpen, setPagesMenuOpen] = useState(false);
+  const [userMenuOpen, setUserMenuOpen] = useState(false);
+
+  const togglePagesMenu = () => setPagesMenuOpen((prev) => !prev);
+  const toggleUserMenu = () => setUserMenuOpen((prev) => !prev);
+  const closeUserMenu = () => setUserMenuOpen(false);
+
+  const goToPage = (path: string) => {
+    navigate(path);
+    setPagesMenuOpen(false);
+  };
 
   const handleDeleteAccount = async () => {
     if (!window.confirm(`Would you like to delete your account?`)) {
-      return closeMenu();
+      return closeUserMenu();
     }
 
     try {
@@ -32,20 +43,37 @@ function Header() {
 
   return (
     <header>
-      <h1>MAJOR</h1>
+      <h1 onClick={togglePagesMenu}>MAJOR</h1>
+
+      {user && (
+        <div className={`pages-menu ${pagesMenuOpen ? "open" : ""}`}>
+          <h2
+            className={location.pathname === "/dashboard" ? "active" : ""}
+            onClick={() => goToPage("/dashboard")}
+          >
+            Dashboard
+          </h2>
+          <h2
+            className={location.pathname === "/activity-logs" ? "active" : ""}
+            onClick={() => goToPage("/activity-logs")}
+          >
+            Activity Logs
+          </h2>
+        </div>
+      )}
 
       {user && (
         <div>
-          <div className="user-box" onClick={toggleMenu}>
+          <div className="user-box" onClick={toggleUserMenu}>
             <AccountCircleIcon />
             <p className="user-name">Hello, {user.name}!</p>
             <div>
-              {menuOpen && (
+              {userMenuOpen && (
                 <div className="user-menu">
                   <button
                     onClick={() => {
                       logout();
-                      closeMenu();
+                      closeUserMenu();
                     }}
                   >
                     <LogoutIcon className="logout-icon" /> Log out
